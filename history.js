700b440010ebc25a7403ccbcfc1433af3ff0c35c
yuvrajdip <dipbhowmik2018@gmail.com>
2022-11-14 16:45:42 +0600

    latest commit

---
+++ history.js
@@ -0,0 +1,41 @@
+const histories = document.getElementById("histories");
+histories.classList.add("container-grid-blogs");
+
+function addHistory(questionText, timeTaken, errorCount) {
+	const newRow = document.createElement("div");
+	newRow.classList.add("card");
+
+	newRow.innerHTML = `
+  <h3>${questionText}</h3>
+  <div>
+  <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+  <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  </div>
+  `;
+
+	histories.appendChild(newRow);
+
+	let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+	previousTests.push({ questionText, timeTaken, errorCount });
+	localStorage.setItem("testHistory", JSON.stringify(previousTests));
+
+	displayHistory();
+}
+
+function displayHistory() {
+	histories.innerHTML = "";
+	const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+
+	previousTests.forEach((test) => {
+		const newRow = document.createElement("div");
+		newRow.classList.add("card");
+
+		newRow.innerHTML = `
+      <p>${test.questionText}</p>
+      <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+      <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    `;
+
+		histories.appendChild(newRow);
+	});
+}
